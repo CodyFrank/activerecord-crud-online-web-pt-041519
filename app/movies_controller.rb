8de163703a15606cbf4156f130971d0fe97668b4
Cody Frank <cody.frank30@gmail.com>
2019-07-06 13:42:04 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,7 +29,8 @@ def can_be_created_in_a_block(args = nil)
   # release_date == 1990
   if args = nil
     Movie.create do |m|
-      m.title = "title"
+      m.title = "Home Alone"
+      m.release_date = 1990
     end
   end
 end
